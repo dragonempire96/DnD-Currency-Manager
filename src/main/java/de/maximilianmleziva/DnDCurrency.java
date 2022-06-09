4b111a809af469a831ea0d392ae2fe1525e26a77
dragonempire96 <maximilian.mleziva@gmx.de>
2022-06-09 12:59:30 +0200

    change class DnDCurrency

--- src/main/java/de/maximilianmleziva/DnDCurrency.java
+++ src/main/java/de/maximilianmleziva/DnDCurrency.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class DnDCurrency {
     public static void main(String[] args) {
-        List<String> money = new Event("Max", 1, 2, 3, 4).asList();
-        System.out.println(money);
+        System.out.println("Hello World!");
     }
 }
