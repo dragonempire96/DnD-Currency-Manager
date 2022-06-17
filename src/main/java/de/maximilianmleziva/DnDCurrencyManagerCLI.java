2e0b926a6f6899f3b122e8bb3f1d7ee723a994a0
dragonempire96 <maximilian.mleziva@gmx.de>
2022-06-18 00:41:38 +0200

    debuged CLI and added missing tests

--- src/main/java/de/maximilianmleziva/DnDCurrencyManagerCLI.java
+++ src/main/java/de/maximilianmleziva/DnDCurrencyManagerCLI.java
@@ -9,7 +9,6 @@ public class DnDCurrencyManagerCLI {
 
     private static final Options options = new Options();
 
-
     public static void main(String[] args) throws IOException {
         DnDCurrencyManagerCLI dnDCurrencyManagerCLI = new DnDCurrencyManagerCLI();
         dnDCurrencyManagerCLI.readCLI(args);
@@ -25,15 +24,19 @@ public class DnDCurrencyManagerCLI {
         Option buyOption = new Option("b", "buy", true, "Kaufe ein Item");
         buyOption.setArgs(2);
         buyOption.setValueSeparator(',');
+        options.addOption(buyOption);
         Option addOption = new Option("a", "add", true, "füge gefundenes oder bekommenes Geld, ausserhalb vom handeln, hinzu");
-        addOption.setArgs(6);
+        addOption.setArgs(5);
+        options.addOption(addOption);
         addOption.setValueSeparator(',');
         Option removeOption = new Option("r", "remove", true, "entferne Geld, ausserhalb vom handeln, von der Liste");
-        removeOption.setArgs(6);
+        removeOption.setArgs(5);
         removeOption.setValueSeparator(',');
+        options.addOption(removeOption);
         Option initOption = new Option("i", "initialize", true, "Füge einen Spieler hinzu");
-        initOption.setArgs(6);
+        initOption.setArgs(5);
         initOption.setValueSeparator(',');
+        options.addOption(initOption);
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd;
@@ -42,23 +45,42 @@ public class DnDCurrencyManagerCLI {
             cmd = parser.parse(options, args);
             if (cmd.hasOption("b")) {
                 TextFile.read();
-                String player = cmd.getOptionValue("b");
-                String item = cmd.getOptionValue("b");
+                String[] buyArgs = cmd.getOptionValues("b");
+                String player = buyArgs[0];
+                String item = buyArgs[1];
                 Buy.buyItem(player, item);
                 System.out.println("gekauft: " + item);
                 return CLIStatus.BUY;
             } else if (cmd.hasOption("a")) {
+                TextFile.read();
+                String[] options = cmd.getOptionValues("a");
+                String player = options[0];
+                List<Integer> money = List.of(Integer.parseInt(options[1]),
+                        Integer.parseInt(options[2]),
+                        Integer.parseInt(options[3]),
+                        Integer.parseInt(options[4]));
+                //Currency.add(player, money);
+                System.out.println("Hinzugefügt: " + money);
                 return CLIStatus.ADD;
             } else if (cmd.hasOption("r")) {
+                TextFile.read();
+                String[] options = cmd.getOptionValues("r");
+                String player = options[0];
+                List<Integer> money = List.of(Integer.parseInt(options[1]),
+                        Integer.parseInt(options[2]),
+                        Integer.parseInt(options[3]),
+                        Integer.parseInt(options[4]));
+                //Currency.remove(player, money);
+                System.out.println("Entfernt: " + money);
                 return CLIStatus.REMOVE;
             } else if (cmd.hasOption("i")) {
+                TextFile.read();
                 String[] options = cmd.getOptionValues("i");
                 String player = options[0];
                 List<Integer> money = List.of(Integer.parseInt(options[1]),
                         Integer.parseInt(options[2]),
                         Integer.parseInt(options[3]),
-                        Integer.parseInt(options[4]),
-                        Integer.parseInt(options[5]));
+                        Integer.parseInt(options[4]));
                 CurrencyMap.updateMap(player, money);
                 System.out.println("Spieler hinzugefügt: " + player);
                 return CLIStatus.INITIALIZE;
--- src/test/java/de/maximilianmleziva/DnDCurrencyManagerCLITest.java
+++ src/test/java/de/maximilianmleziva/DnDCurrencyManagerCLITest.java
@@ -12,10 +12,70 @@ class DnDCurrencyManagerCLITest {
 
     private DnDCurrencyManagerCLI dndCurrencyManagerCLI;
 
+    @BeforeEach
+    void init() {
+        this.dndCurrencyManagerCLI = new DnDCurrencyManagerCLI();
+    }
     @Test
     @DisplayName("Leerer Aufruf, zeige Hilfe an.")
     public void test1() throws IOException {
         CLIStatus cliStatus = dndCurrencyManagerCLI.readCLI(new String[]{});
         assertThat(cliStatus).isEqualTo(CLIStatus.HELP);
+    }
+    @Test
+    @DisplayName("Aufruf buy, entfernt Preis des Items.")
+    public void test2() throws IOException {
+        String[] arguments = {
+                "--buy",
+                "Maenix",
+                "Langschwert"
+        };
+        CLIStatus cliStatus = dndCurrencyManagerCLI.readCLI(arguments);
+        assertThat(cliStatus).isEqualTo(CLIStatus.BUY);
+    }
+
+    @Test
+    @DisplayName("Aufruf add, fügt Geld hinzu.")
+    public void test3() throws IOException {
+        String[] arguments = {
+                "--add",
+                "key",
+                "1",
+                "0",
+                "0",
+                "0"
+        };
+        CLIStatus cliStatus = dndCurrencyManagerCLI.readCLI(arguments);
+        assertThat(cliStatus).isEqualTo(CLIStatus.ADD);
+    }
+
+    @Test
+    @DisplayName("Aufruf remove, entfernt Geld.")
+    public void test4() throws IOException {
+        String[] arguments = {
+                "--remove",
+                "key",
+                "1",
+                "0",
+                "0",
+                "0"
+        };
+        CLIStatus cliStatus = dndCurrencyManagerCLI.readCLI(arguments);
+        assertThat(cliStatus).isEqualTo(CLIStatus.REMOVE);
+    }
+
+    @Test
+    @DisplayName("Aufruf init, added Spieler.")
+    public void test5() throws IOException {
+        String[] arguments = {
+                "--initialize",
+                "Maenix",
+                "0",
+                "41",
+                "6",
+                "6"
+        };
+        CLIStatus cliStatus = dndCurrencyManagerCLI.readCLI(arguments);
+        assertThat(cliStatus).isEqualTo(CLIStatus.INITIALIZE);
     }
 }
