cab8971d439f18d6c79a0653082ab401b78607c4
dragonempire96 <maximilian.mleziva@gmx.de>
2022-06-19 02:45:11 +0200

    added TODO to Buy

--- src/main/java/de/maximilianmleziva/Buy.java
+++ src/main/java/de/maximilianmleziva/Buy.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 class Buy {
     static void buyItem(String player,String item) {
+        //TODO: implement buying of items
+        //TODO: check if player has enough money
         List<Integer> price = new ArrayList<>();
         Currency.remove(player, price);
         System.out.println("Buy " + item);
